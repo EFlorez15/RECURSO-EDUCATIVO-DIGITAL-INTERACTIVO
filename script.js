fa2b14190420c891bb4cd469553e84cefe1ae8fa
EFlorez15 <codigosdebarra98@gmail.com>
2026-02-19 19:28:58 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+// Agregar evento al botón de glosario
+document.getElementById('btn-glosario').addEventListener('click', function() {
+ document.getElementById('glosario-contenido').style.display = 'block';
+});
+
+// Agregar contenido al mapa interactivo
+const mapaInteractivo = document.getElementById('mapa-interactivo');
+mapaInteractivo.innerHTML = '<iframe src="https:                                                                       
+
+                                   
+const actividadContenido = document.getElementById('//www.google.com/maps/embed?..." width="100%" height="100%"></iframe>';
+
+// Agregar contenido a la actividad
+const actividadContenido = document.getElementById('actividad-contenido');
+actividadContenido.innerHTML = '<p>Ubica los lugares del relato en el mapa</p>';
